094ade4b90cc10971ce10cbc6264f2bd7cb5cfc6
taylorpk <taylorpk1203@gmail.com>
2023-07-24 13:12:58 -0400

    caesar solution

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
-# add your code here
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+sentence = input('Please enter a sentence: ')
+sentence= sentence.lower()
+
+cipher = ''
+
+for char in sentence:
+    if char in letters: 
+      index = letters.find(char)
+      index = index + 5
+      if index > 25:
+        index -= 26
+      char = letters[index]
+    cipher += char
+
+print('The encrypted sentence is:', cipher)
